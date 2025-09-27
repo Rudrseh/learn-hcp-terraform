9696a6d2d6ccce2c8dc2ad7b0f463be76498b3cd
Rudrseh <32550055+Rudrseh@users.noreply.github.com>
2025-09-27 19:43:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "instance_name" {
 }
 
 variable "instance_type" {
-  description = "The EC2 instance's type is ubuntu "
+  description = "The EC2 instance's type"
   type        = string
   default     = "t3.micro"
 }
